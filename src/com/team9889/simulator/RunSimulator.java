b82a144c371ebfcb08e0dc9f8d91dc345d1febe2
joshua9889 <joshuatheinrich@gmail.com>
2020-06-21 21:33:35 -0400

    Rework?

---
+++ src/com/team9889/simulator/RunSimulator.java
@@ -0,0 +1,9 @@
+package com.team9889.simulator;
+
+public class RunSimulator {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/team9889/simulator/hardware/DcMotor.java
@@ -0,0 +1,5 @@
+package com.team9889.simulator.hardware;
+
+public class DcMotor {
+
+}
